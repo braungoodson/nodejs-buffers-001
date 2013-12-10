6068faca3833660eb8244d93d7ab9661da641cfd
Goodson <braun.goodson@raq10-384.local>
2013-12-09 21:34:34 -0500

    done

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ var iiab = initialized_integer_array_buffer;
 // initialized binary encoded buffer
 var initialized_binary_encoded_buffer = new Buffer("I'm a string!","utf-8");
 var ibeb = initialized_binary_encoded_buffer;
+// write to five bytes of the uninitialized buffer
+// the uninitialized array will contain garbage
+//  each character in the string happens to be 8 bits
+//  8 bits x 5 characters = 40 bits
+//  8 bytes x 8 bits = 64 bit buffer
+//  40 bits / 64 bit buffer = ~2/3 of the buffer
+var b = new Buffer(8);
+b = new Buffer(16);
+console.log(b.write('hello','utf-8'));
+console.log(b.toString());
+console.log(b.write(' world!',5,'utf-8'));
---
+++ test/unit/index.suite.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('nodejs-buffers-001',function(){
+  
+  it('should run',function(){
+    assert(require('../../index.js'));
+  })
+
+})
